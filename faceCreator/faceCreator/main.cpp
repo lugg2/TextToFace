ca106693cb037ae76e025632c955613315c2e204
pat-rick <s122022@student.dhbw-mannheim.de>
2014-03-03 14:35:41 +0100

    Übergabeparameter
    Bild wird erstellt und unter id.png (Übergabeparameter) gespeichert

--- faceCreator/faceCreator/main.cpp
+++ faceCreator/faceCreator/main.cpp
@@ -1,17 +1,26 @@
+#include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
-#include <Image.hpp>
 
-int main()
+
+int main(int argc, char* argv[])
 {
+std::cout << "The name used to start the program: " << argv[ 1 ];
+
+std::string pathname = argv[1];
+pathname += ".png";
+
 // Create a 20x20 image filled with black color
 sf::Image image;
 image.create(20, 20, sf::Color::Black);
 
-// Copy image1 on image2 at position (10, 10)
-//image.copy(background, 10, 10);
+// Copy image1 on image at position (10, 10)
+sf::Image image1;
+image1.create(10,10, sf::Color::Blue);
+image.copy(image1, 10, 10);
 
 // Save the image to a file
-if (!image.saveToFile("result.png"))
+if (!image.saveToFile(pathname))
     return -1;
 
 }
