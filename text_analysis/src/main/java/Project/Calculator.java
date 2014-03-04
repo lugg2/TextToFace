b8528421b45dae5bc865c3c9c64ca0bf13a57aeb
Einalem <Melanie.Hammerschmidt@online.de>
2014-03-04 16:00:23 +0100

    TA: detection of vocals

--- text_analysis/src/main/java/Project/Calculator.java
+++ text_analysis/src/main/java/Project/Calculator.java
@@ -1,10 +1,14 @@
 package Project;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.languagetool.JLanguageTool;
 import org.languagetool.language.German;
+import org.languagetool.rules.Rule;
 import org.languagetool.rules.RuleMatch;
 
 public class Calculator {
@@ -36,19 +40,64 @@ public class Calculator {
 		}
 	
 		for (RuleMatch match : matches) {
-			/*System.out.println("Potential error at line " +
-					match.getLine() + ", column " +
-					match.getColumn() + ": " + match.getMessage());
-			System.out.println("Suggested correction: " +
-					match.getSuggestedReplacements());*/
 			numb_errors++;
 		}
 		
 		//count all sentences
 		int numb_sentences = langTool.getSentenceCount();
 		
+		//count all vocals
+		int numb_vocals = 0;
+		Pattern p = Pattern.compile("[*a*|*e*|*i*|*o*|*u*|*A*|*E*|*I*|*O*|*U*]");
+		Matcher m = p.matcher(enteredText);
+
+		List<String> vocals = new ArrayList<String>();
+		while (m.find()) {
+			numb_vocals++;
+			vocals.add(m.group());
+		}
+		
+		//check for word of the class xx
+		int numb_xx = 0;
+		
+		disableAllRules(langTool);
+		langTool.enableDefaultOffRule("xx_RULE");
+		
+		try {
+			langTool.check(enteredText);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		for (RuleMatch match : matches) {
+			numb_xx++;
+		}
+			
 		//stored information
 		System.out.println("sentences:" + numb_sentences);
 		System.out.println("errors:" + numb_errors);
+		System.out.println("vocals:" + numb_vocals);
+		//..
+		System.out.println("xx:" + numb_xx);
+	}
+	
+	private void disableAllRules(JLanguageTool lang)
+	{
+		lang.disableCategory("PERSONAL_EXTENDS");		
+		lang.disableCategory("Mögliche Tippfehler");
+		lang.disableCategory("Leicht zu verwechselnde Wörter");
+		lang.disableCategory("Falschschreibung prominenter/geographischer Eigennamen");
+		lang.disableCategory("Zusammen-/Getrenntschreibung");
+		lang.disableCategory("Semantische Unstimmigkeiten");
+		lang.disableCategory("Redundanz");
+		lang.disableCategory("Stil, Umgangssprache");
+		lang.disableCategory("Briefe und E-Mails");
+		lang.disableCategory("Groß-/Kleinschreibung");
+		lang.disableCategory("Grammatik");
+		lang.disableCategory("Redewendungen");
+		lang.disableCategory("Zeichensetzung");
+		lang.disableCategory("Typographie");
+		lang.disableCategory("Sonstiges");
+		lang.disableCategory("Wikipedia");
 	}
 }
