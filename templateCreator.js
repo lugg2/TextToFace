1a984230486efd501c3008bfe5eb2dcbfb354868
vschwart <victor.schwartz@gmx.de>
2014-04-18 11:35:30 +0200

    blabla

--- templateCreator.js
+++ templateCreator.js
@@ -14,14 +14,19 @@ function generateTemplate(idToken, publickeyToken)
 	fs.readFile('website/result.html',"utf-8", function (err, data) {
 	if (err) console.log("feheler");
 	
-	data = data.replace("<BILD>","<img src=" + '"' + idToken +".png"+'"'+"width = 80% > ");
+	data = data.replace("<BILD>","<img src=" + '"' + idToken +".png"+'"'+"width = 40% > ");
 	
 	fs.readFile('wantedPoster/wantedPoster'+id,"utf-8", function (err, wantedPoster) {
 	if (err) console.log("feheler");
 	
-	data = data.replace("<WantedPoster>",wantedPoster);
+	data = data.replace("<WANTEDPOSTER>",wantedPoster);
 	console.log(data);
+	fs.writeFile(__dirname + '/website/result' + id+'.html', data, function(err) {
+		//callback(err);
+	}); 
 		});
+	
+	
 	
 	});
 	//console.log(data);
