9e2285d45733b1ab8d8281cb7f7c10ba03118997
T3nj1 <akira6529@yahoo.comakira6529@yahoo.com>
2014-04-18 16:12:30 +0200

    Text des Button wechselt beim Draufklicken
    mit Knockouts
    benutzte Tag <button> anstatt vorher <input>

---
+++ website/js/ttf.js
@@ -0,0 +1,18 @@
+var ClickCounterViewModel = function(names) {
+    this.numberOfClicks = ko.observable(0);
+    this.names = ko.observableArray(names)
+ 
+    this.registerClick = function() {
+        this.numberOfClicks(this.numberOfClicks() + 1);
+    };
+ 
+    this.resetClicks = function() {
+        this.numberOfClicks(0);
+    };
+ 
+    this.hasClickedTooManyTimes = ko.computed(function() {
+        return this.numberOfClicks() >= 5;
+    }, this);
+};
+ 
+ko.applyBindings(new ClickCounterViewModel(["Upload","In der Warteschlange","Wurde analysiert","Der Künstler sucht nach Inspiration","Der Künstler ist fertig","Sie werden weitergeleitet"]));
