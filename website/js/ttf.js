e6412042dcd6ef1fd55545cf145e3522885ad5b3
T3nj1 <akira6529@yahoo.comakira6529@yahoo.com>
2014-04-18 16:50:50 +0200

    Die Javascript-Datei im letzten Commit vergessen

--- website/js/ttf.js
+++ website/js/ttf.js
@@ -13,6 +13,14 @@ var ClickCounterViewModel = function(names) {
     this.hasClickedTooManyTimes = ko.computed(function() {
         return this.numberOfClicks() >= 5;
     }, this);
+	
+	this.fertig = ko.computed(function() {
+		return this.numberOfClicks() >=4;
+	}, this);
+	
+	this.nichtFertig = ko.computed(function() {
+		return this.numberOfClicks() <4;
+	}, this);
 };
  
 ko.applyBindings(new ClickCounterViewModel(["Upload","In der Warteschlange","Wurde analysiert","Der Künstler sucht nach Inspiration","Der Künstler ist fertig","Sie werden weitergeleitet"]));
