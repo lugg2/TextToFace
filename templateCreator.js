ad06f9dee87c7cc8c84b62895ddcd39835378eff
vschwart <victor.schwartz@gmx.de>
2014-04-29 11:36:14 +0200

    text unter dem bild

--- templateCreator.js
+++ templateCreator.js
@@ -29,16 +29,24 @@ function generateTemplate(response, request)
 	data = data.replace("<BILD>","<img src=" + 'pictures/face'+ idToken +".png"+' '+"width = 40% > ");
 	
 
-	fs.readFile(__dirname +'/wantedPoster/wantedPoster'+idToken,"utf-8", function (err, wantedPoster) {
+		fs.readFile(__dirname +'/wantedPoster/wantedPoster'+idToken,"utf-8", function (err, wantedPoster) {
 
-	if (err) console.log("could not find wanted Poster");
+			if (err) console.log("could not find wanted Poster");
 	
-	data = data.replace("<WANTEDPOSTER>",wantedPoster);
-        response.writeHead(200, {'content-type': 'text/html'});
-        response.write(data);
-        response.end();
-	});
+			fs.readFile(__dirname +'/text_analysis/Codemsg/message'+idToken,"utf-8", function (err, message) {
+
+			if (err) console.log("could not find wanted message");
 	
+	
+	
+			data = data.replace("<WANTEDPOSTER>",wantedPoster);
+			data = data.replace("<MESSAGE>",message);
+			
+				response.writeHead(200, {'content-type': 'text/html'});
+				response.write(data);
+				response.end();
+			});
+		});
 	});
 
 }
